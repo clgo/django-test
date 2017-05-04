ad8638cd59bffc63295ecb0744c6e227278239bb
clgo <clgo2005@gmail.com>
2017-05-04 21:40:43 +0800

    First Commit

---
+++ polls/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.index, name="index"),
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+	#127.0.0.1/polls/1
+	url(r'^(?P<question_id>[0-9]+)/results$', views.results, name="results"),
+	#127.0.0.1/polls/1/results
+	url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name="volt"),
+	#127.0.0.1/polls/1/votes
+]
---
+++ polls/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Question
+
+# Create your views here.
+def index(request):
+	latest_questions = Question.objects.order_by('-pub_date')[:5]
+	#output = ", ".join(q.question_text for q in latest_questions)
+	context = {
+		'latest_questions': latest_questions
+	}
+	
+	#return HttpResponse('Latest Question are: %s' % output)
+	return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+	question = Question.objects.get(pk = question_id)
+	return render(request, 'polls/detail.html', { 'question' : question })
+
+def results(request, question_id):
+	return HttpResponse("These are the results of the question: %s" % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("Vote on question: %s" % question_id)
